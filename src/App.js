0d18696d829f5a5d82f0eb6966308703b7b66373
thauandev <thauandepaula@icloud.com>
2020-04-30 11:08:24 -0300

    Adicionando like ao state local

--- src/App.js
+++ src/App.js
@@ -15,28 +15,40 @@ import {
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  async function handleLikeRepository(id) {
-    await api.post(`repositories/${id}/like`);
+  /** Na lógica abaixo eu busco no meu state local por meio do map e incremento o like em 1
+   * caso o ID seja igual ao ID que o usuário clicou e não dependo de atualizar a lista de
+   * repositórios com base no api.get('/repositories')
+   */
+  
+   async function handleLikeRepository(repositorie) {
+    await api.post(`repositories/${repositorie.id}/like`);
 
+    const updateRepositories = repositories.map((r)=>{
+      if (r.id === repositorie.id) {
+        r.likes += 1;
+      }
+      return r;
+    })
+     setRepositories(updateRepositories);
   }
 
   useEffect(()=>{
     api.get('repositories').then(response =>{
       setRepositories(response.data)
     })
-  })
+  },[])
 
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        {repositories.map(repositorie => (
-          <View style={styles.repositoryContainer} key={repositorie.id}>
-          <Text style={styles.repository}>{repositorie.title}</Text>
+        {repositories.map(({id, techs, likes, title}) => (
+          <View style={styles.repositoryContainer} key={id}>
+          <Text style={styles.repository}>{title}</Text>
 
           <View style={styles.techsContainer}>
-            {repositorie.techs.map(tech => <Text key={tech} style={styles.tech}>
+            {techs.map(tech => <Text key={tech} style={styles.tech}>
                 {tech}
               </Text> )}
             
@@ -46,17 +58,17 @@ export default function App() {
             <Text
               style={styles.likeText}
               // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repositorie-likes-${repositorie.id}`}
+              testID={`repository-likes-${id}`}
             >
-              {`${repositorie.likes} curtidas`}
+              {likes} curtida{likes > 1 ? 's' : ''}
             </Text>
           </View>
 
           <TouchableOpacity
             style={styles.button}
-            onPress={() => handleLikeRepository(repositorie.id)}
+            onPress={() => handleLikeRepository({id, techs, likes, title})}
             // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-${repositorie.id}`}
+            testID={`like-button-${id}`}
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
